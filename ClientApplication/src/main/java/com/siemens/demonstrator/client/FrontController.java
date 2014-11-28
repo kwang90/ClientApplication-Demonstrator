f6f7d64beb1fe3b76215e9347ab9c3b690e82a19
Ahmed Shafei <Ahmed.Shafei@tum.de>
2014-11-28 05:24:05 +0100

    initial project commit

--- ClientApplication/src/main/java/com/siemens/demonstrator/client/FrontController.java
+++ ClientApplication/src/main/java/com/siemens/demonstrator/client/FrontController.java
@@ -1,19 +1,37 @@
 package com.siemens.demonstrator.client;
 
+import java.util.Locale;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class FrontController {
 
-	@RequestMapping("/hello")
-	public String hello(
-			@RequestParam(value = "name", required = false, defaultValue = "World") String name,
-			Model model) {
-		model.addAttribute("name", name);
-		return "helloworld";
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView home(Locale locale, Model model) {
+		System.out.println("home");
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("index");
+		mv.addObject("name", "ahmed");
+		return mv;
 	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView loginPage(Locale locale, Model model) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("index");
+		mv.addObject("name", "ahmed");
+		return mv;
+	}
+
+	/*
+	 * @RequestMapping(value = "/home", method = RequestMethod.POST) public
+	 * String login(@Validated User user, Model model) {
+	 * model.addAttribute("userName", user.getUserName()); return "user"; }
+	 */
 
 }
